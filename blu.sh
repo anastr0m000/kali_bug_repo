c3fc1e23cbfdcf7acc67397e3608df7793e71520
anastr0m000 <anastr0m000@gmail.com>
2024-08-16 04:46:26 -0400

    Update blu.sh

--- blu.sh
+++ blu.sh
@@ -1,9 +1,5 @@
 ls
 # nuclei
 cp /root/noVNC/vnc.html /root/noVNC/index.html
-# git clone https://github.com/r0oth3x49/ghauri.git
-# cd ghauri
-# python3 -m pip3 install --upgrade -r requirements.txt
-# python3 setup.py install
-# cd ..
+
 ls
