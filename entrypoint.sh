63081ae9d575cfdef09a4c4bd4d44f18893550db
anastr0m000 <anastr0m000@gmail.com>
2024-08-16 04:19:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,6 +35,8 @@ echo "xuser  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo 'xfce4-terminal -e "sudo -i"' >> /home/xuser/Desktop/terminal.sh
 chmod +x /home/xuser/Desktop/terminal.sh
 mkdir /root/hunt
+wget https://raw.githubusercontent.com/tamimhasan404/Wordlist404/main/Subdomain-Brute-Force/subdomain-bruteforce.txt -O /root/hunt/subdomain-bruteforce.txt
+
 source ~/.profile
 
 supervisord 1>/dev/null 2>/dev/null
