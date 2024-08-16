19252b197b7ff8f28ff39cec8a6820869e1834af
anastr0m000 <anastr0m000@gmail.com>
2024-08-16 05:51:12 -0400

    Update blue2.sh

--- blue2.sh
+++ blue2.sh
@@ -4,5 +4,9 @@ cd ghauri
 python3 -m pip3 install --upgrade -r requirements.txt
 python3 setup.py install
 cd ..
+git clone https://github.com/0x240x23elu/JSScanner.git
+cd JSScanner
+pip3 install -r  requirements.txt
+cd ..
 
 
