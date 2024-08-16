776ea9ed28ca59c2181475794dec711ae137791f
anastr0m000 <anastr0m000@gmail.com>
2024-08-16 03:14:14 -0400

    Update blue2.sh

--- blue2.sh
+++ blue2.sh
@@ -1,6 +1,4 @@
 ls
-mkdir /root/hunt
+
 wget https://raw.githubusercontent.com/tamimhasan404/Wordlist404/main/Subdomain-Brute-Force/subdomain-bruteforce.txt -O /root/hunt/subdomain-bruteforce.txt
-echo "xuser  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo 'xfce4-terminal -e "sudo -i"' >> /home/xuser/Desktop/terminal.sh
-chmod +x /home/xuser/Desktop/terminal.sh
+
