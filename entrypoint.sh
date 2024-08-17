d70b55510fc4fc22057770fea5cdadf36020a28c
anastr0m000 <anastr0m000@gmail.com>
2024-08-16 20:46:00 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,10 @@ echo "export GOPATH=$HOME/go" >> ~/.profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.profile
 
 echo "xuser  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo 'xfce4-terminal -e "sudo -i"' >> /home/xuser/Desktop/terminal.sh
-chmod +x /home/xuser/Desktop/terminal.sh
+mkdir -p /home/xuser/Desktop/
+echo "xfce4-terminal -e 'sudo -i'" >> /home/xuser/Desktop/terminal.sh
+chmod 600 /home/xuser/Desktop/terminal.sh
+chown -R xuser:xuser /home/xuser/
 mkdir /root/hunt
 wget https://raw.githubusercontent.com/tamimhasan404/Wordlist404/main/Subdomain-Brute-Force/subdomain-bruteforce.txt -O /root/hunt/subdomain-bruteforce.txt
 
