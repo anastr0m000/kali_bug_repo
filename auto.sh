89bf4ef55465a7a29fa8cb767767fc5a6e6438be
anastr0m000 <anastr0m000@gmail.com>
2024-08-17 03:20:54 -0400

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -43,18 +43,21 @@ if [[ -n "$TARGET_ARG" ]]; then
     echo "Argument after -u: '$TARGET_ARG'"
 else  
     echo "No argument provided after -u."
+	TARGET_ARG="rewire.com"
 fi
 
 
-TARGET_ARG="rewire.com"
-mkdir $TARGET_ARG
+mkdir -p $TARGET_ARG
 cd $TARGET_ARG"/"
-subfinder -d $TARGET_ARG -all | anew subs.txt
+subfinder -d $TARGET_ARG -all -recursive | anew subs.txt
+echo $TARGET_ARG | gau| grep -Ev '\.(jpg|png|gif)$' | anew subs.txt
+cat subs.txt | httpx -ports 80,443,8080,8000,8888 -threads 200 |anew live.txt
 
 # shuffledns -d $TARGET_ARG  -r resolvers.txt -w nokovo_subdomains_huge.txt | anew subs.txt
 
 # dnsx -1 subs.txt -r resolvers.txt | anew resolved.txt
 
 # naabu -1 resolved.txt -nmap -rate 5000 | anew ports.txt; httpx -l ports .txt | anew alive.txt 
-katana -list subs.txt -kf all -jc | anew urls.txt 
+katana -list live.txt -kf all -jcn -d 5 -ps -pss waybackarchive,commoncrawl,alienvault -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg -o urls.txt 
+
 nuclei -l urls.txt -es info, unknown -ept ssl -ss template-spray | anew nuclei.txt
